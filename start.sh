d310022f13d561f8c98111e989081f9fb045bc0a
shirquille.sande <shirquille.sande@student.moringaschool.com>
2021-08-25 11:15:34 +0300

    db update

--- start.sh
+++ start.sh
@@ -2,4 +2,4 @@ export SECRET_KEY='12345'
 export MAIL_USERNAME='shirquille.sande@student.moringaschool.com'
 export MAIL_PASSWORD='@moringa2021'
 
-python3 manage.py server 
+python3 manage.py server      
